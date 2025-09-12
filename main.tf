39b6e593254d28f37da8391a96f849839f70689b
kainze <73406030+kainze@users.noreply.github.com>
2025-09-12 14:52:28 +0200

    init

---
+++ main.tf
@@ -0,0 +1,79 @@
+# Tell Terraform to include the hcloud provider
+terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "1.52.0"
+    }
+  }
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+resource "hcloud_network" "private_network" {
+  name     = "kubernetes-cluster"
+  ip_range = "10.0.0.0/16"
+}
+
+resource "hcloud_network_subnet" "private_network_subnet" {
+  type         = "cloud"
+  network_id   = hcloud_network.private_network.id
+  network_zone = "eu-central"
+  ip_range     = "10.0.1.0/24"
+}
+
+output "network_id" {
+  value = hcloud_network.private_network.id
+}
+
+resource "hcloud_ssh_key" "me" {
+  name       = "iu-k8s-key"
+  public_key = file(var.ssh_public_key_path)
+}
+
+# Master
+resource "hcloud_server" "master-node" {
+  name        = "master-node"
+  image       = var.image
+  server_type = var.server_type
+  location    = var.location
+  ssh_keys    = [hcloud_ssh_key.me.id]
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+  network {
+    network_id = hcloud_network.private_network.id
+    ip         = "10.0.1.1"
+  }
+  user_data = templatefile("${path.module}/cloud-init-master.tmpl", {
+    pc_public_key     = trimspace(file(var.ssh_public_key_path))
+    worker_public_key = trimspace(file(var.ssh_worker_public_key_path))
+  })
+  depends_on = [hcloud_network_subnet.private_network_subnet]
+}
+
+# Worker
+resource "hcloud_server" "worker-nodes" {
+  count       = var.worker_count
+  name        = "worker-node-${count.index}"
+  image       = var.image
+  server_type = var.server_type
+  location    = var.location
+  ssh_keys    = [hcloud_ssh_key.me.id]
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+  network {
+    network_id = hcloud_network.private_network.id
+  }
+  user_data = templatefile("${path.module}/cloud-init-worker.tmpl", {
+    pc_public_key      = trimspace(file(var.ssh_public_key_path))
+    worker_private_key = indent(6, file(var.ssh_worker_private_key_path))
+  })
+
+  depends_on = [hcloud_network_subnet.private_network_subnet, hcloud_server.master-node]
+}
---
+++ variables.tf
@@ -0,0 +1,58 @@
+variable "hcloud_token" {
+  description = "Hetzner Cloud API token"
+  type        = string
+  sensitive   = true
+}
+
+variable "k3s_token" {
+  description = "Shared token for k3s cluster (server/workers)"
+  type        = string
+  sensitive   = true
+}
+
+variable "location" {
+  description = "Hetzner location (fsn1, nbg1, hel1)"
+  type        = string
+  default     = "fsn1"
+}
+
+variable "server_type" {
+  description = "Hetzner server type"
+  type        = string
+  default     = "cax11"
+}
+
+variable "image" {
+  description = "Base image"
+  type        = string
+  default     = "ubuntu-24.04"
+}
+
+variable "worker_count" {
+  description = "Number of workers"
+  type        = number
+  default     = 2
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to your local public SSH key"
+  type        = string
+  default     = "~/.ssh/id_ed25519.pub"
+}
+
+variable "my_public_key" {
+  description = "my ssh public key"
+  type        = string
+  sensitive   = true
+}
+variable "ssh_worker_public_key_path" {
+  description = "Path to worker public key"
+  type        = string
+  default     = "~/.ssh/id_ed25519_k8s_worker.pub"
+}
+
+variable "ssh_worker_private_key_path" {
+  description = "Path to worker private key"
+  type        = string
+  default     = "~/.ssh/id_ed25519_k8s_worker"
+}
